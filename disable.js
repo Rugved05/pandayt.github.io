c239be805ca624629ee4b449da7f2a8f52663979
PandaYT <99893694+Rugved05@users.noreply.github.com>
2022-02-19 22:10:38 +0530

    Add files via upload

---
+++ disable.js
@@ -0,0 +1,24 @@
+var message="Sorry,Right click disabled. Message from Panda YT";
+///////////////////////////////////
+function clickIE() {if (document.all) {alert(message);return false;}}
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {alert(message);return false;}}}
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+
+document.oncontextmenu=new Function("return false")
+// -->
+
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
--- script.js
+++ script.js
@@ -87,3 +87,12 @@ function fadeOut(){
 }
 
 window.onload = fadeOut;
+if (typeof document.onselectstart!="undefined")
+{
+  document.onselectstart=new Function ("return false");
+}
+else{
+  document.onmousedown=new Function ("return false")
+  document.onmouseup=new Function ("return false")
+  
+}
